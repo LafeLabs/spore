70814bb8697de2c5614ee2a7a4f652ccc776cc70
Trash Robot <lafelabs@gmail.com>
2026-07-11 19:38:22 -0600

    fix the replication cycle

--- spore.php
+++ spore.php
@@ -1,17 +1,14 @@
 <?php
-$spore = "https://raw.githubusercontent.com/LafeLabs/spore/refs/heads/main/spore.php";
-$baseurl = explode("spore.php",$spore)[0];
+$sporeUrl = "https://raw.githubusercontent.com/LafeLabs/spore/refs/heads/main/spore.json";
 
-@copy($baseurl."index.html","index.html");
-@copy($baseurl."editor.html","editor.html");
-@copy($baseurl."save-file.php","save-file.php");
-@copy($baseurl."save-file-get.php","save-file-get.php");
-@copy($baseurl."load-file.php","load-file.php");
-@copy($baseurl."list-files.php","list-files.php");
-@copy($baseurl."list-directories.php","list-directories.php");
-@copy($baseurl."README.md","README.md");
-@copy($baseurl."spore.php","spore.php");//comment out to keep existing spore
-@copy($baseurl."wall.txt","wall.txt");//comment out to keep existing spore
+$baseUrl = explode("spore.json",$sporeUrl)[0];
+
+$files = json_decode(file_get_contents($sporeUrl), true);
+
+foreach ($files as $file) {
+    @copy($baseUrl.$file,$file);
+}
+
 
 ?>
 <a href = "index.html">index.html</a>
