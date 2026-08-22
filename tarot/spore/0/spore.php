7363f5c06dec3268ccef1a6e759ef3d2eb57c4e9
Trash Robot <lafelabs@gmail.com>
2026-08-21 18:00:29 -0600

    TAROT RECURSION

---
+++ tarot/spore/0/spore.php
@@ -0,0 +1,21 @@
+<?php
+$sporeUrl = "https://raw.githubusercontent.com/LafeLabs/spore/refs/heads/main/spore.json";
+
+$baseUrl = explode("spore.json",$sporeUrl)[0];
+
+$files = json_decode(file_get_contents($sporeUrl), true);
+
+foreach ($files as $file) {
+    @copy($baseUrl.$file,$file);
+}
+
+
+?>
+<a href = "index.html">index.html</a>
+<style>
+a{
+    font-size:3em;
+    color:blue;
+    font-family:arial;
+}
+</style>
