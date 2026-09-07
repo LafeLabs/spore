adcb8ee8169cfb30ec3bce05e44a6659d172c3c1
Trash Robot <lafelabs@gmail.com>
2026-09-07 12:27:26 -0400

    Update spore.php

--- spore.php
+++ spore.php
@@ -1,5 +1,5 @@
 <?php
-    $sporeUrl = "https://raw.githubusercontent.com/LafeLabs/spore/refs/heads/main/python/spore.json";
+    $sporeUrl = "https://raw.githubusercontent.com/LafeLabs/spore/refs/heads/main/spore.json";
     $baseUrl = explode("spore.json",$sporeUrl)[0];
     $spore = json_decode(file_get_contents($sporeUrl), true);
     $files = $spore['files'];    
